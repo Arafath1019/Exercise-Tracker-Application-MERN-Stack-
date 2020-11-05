f526c138d55c5dc5b433bf17ea5ee83a69f609b6
Arafath1019 <arafath.yeasin1019@gmail.com>
2020-11-05 11:03:44 +0600

    Adding models and routes in backend

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,9 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+
+
+// DB Coonection
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
 const connection = mongoose.connection;
@@ -17,6 +20,14 @@ connection.once('open', () => {
     console.log("MongoDB database connection established successfully");
 });
 
+// Routes
+const exercisesRouter = require('./routes/exercises');
+const usersRouter = require('./routes/users');
+
+app.use('/exercises', exercisesRouter);
+app.use('/users', usersRouter);
+
+// Server Listening
 app.listen(port, () => { 
     console.log(`Server is running on port: ${port}`);
 }); 
